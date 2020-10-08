55a91cef952b10dc7b3f9b689ef338762ed5be3b
Hongyu Cai <h.tsai@hotmail.com>
2020-10-08 15:47:31 +0800

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 wget http://gosspublic.alicdn.com/ossutil/1.6.19/ossutil64
 chmod 755 ossutil64
 ./ossutil64 config -e oss-cn-beijing.aliyuncs.com -i $ACCESSKEY -k $SECRETKEY
-./ossutil64 rm -rf oss://care-www/ --include "*"
-./ossutil64 cp -f ./dist/ --recursive --include "*" oss://care-www/
+./ossutil64 rm -rf oss://care-www/ --include "*.html"
+./ossutil64 cp -f ./dist/ --recursive --include "*.html" oss://care-www/
