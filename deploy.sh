d8697c5a735f08c07270daf00c44e64a4c1cd7c5
Hongyu Cai <h.tsai@hotmail.com>
2020-10-23 13:15:44 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@ chmod 755 ossutil64
 ./ossutil64 config -e oss-cn-beijing.aliyuncs.com -i $ACCESSKEY -k $SECRETKEY
 ./ossutil64 rm -rf oss://care-www/ --include "*.html"
 ./ossutil64 cp -f ./dist/ --recursive --include "*.html" oss://care-www/
+./ossutil64 cp -f ./dist/assets/js/theme.min.js oss://care-www/assets/js/
