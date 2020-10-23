3033ae0c8059faf3c8db8c0e40e109d3affcd261
Hongyu Cai <h.tsai@hotmail.com>
2020-10-23 14:49:25 +0800

    Update English

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,4 @@ chmod 755 ossutil64
 ./ossutil64 rm -rf oss://care-www/ --include "*.html"
 ./ossutil64 cp -f ./dist/ --recursive --include "*.html" oss://care-www/
 ./ossutil64 cp -f ./dist/assets/js/theme.min.js oss://care-www/assets/js/
+./ossutil64 cp -f ./dist/assets/css/theme.min.css oss://care-www/assets/css/
