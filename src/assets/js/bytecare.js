0125a55b8767cfa40c70c2efe988cd1b78ec4abf
Hongyu Cai <h.tsai@hotmail.com>
2020-10-23 12:33:55 +0800

    Chinese

--- src/assets/js/bytecare.js
+++ src/assets/js/bytecare.js
@@ -5,26 +5,28 @@
 
 'use strict';
 
-(function() {
+(function () {
 
-  //
-  // Functions
-  //
+    //
+    // Functions
+    //
 
-  function init() {
-    const token = Cookies.get('token');
-    if (token) {
-       $("#bytecare-login-btn").hide();
-       $("#bytecare-console-btn").show();
+    function init() {
+        const token = Cookies.get('token');
+        if (token) {
+            $("#bytecare-login-btn").hide();
+            $("#bytecare-console-btn").show();
+            $("#bytecare-login-btn-nav").hide();
+            $("#bytecare-console-btn-nav").show();
+        }
     }
-  }
 
 
-  //
-  // Events
-  //
+    //
+    // Events
+    //
 
 
     init();
-  
+
 })();
